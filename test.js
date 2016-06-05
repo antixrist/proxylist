e245d87dc67001025df9be89162c095d2df26c94
self_refactor <deyan8284@gmail.com>
2016-06-05 23:24:28 +0000

    update

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
 "use strict"
 const expect = require("unexpected")
+const R = require("ramda")
 const tested = require("./proxylist")
 describe("proxylist", () => {
     it("main", (done) => {
         tested.main().then((incoming) => {
             expect(incoming, "to be an array")
             expect(incoming.length, "to be greater than", 5)
-            expect(incoming[0], "to contain", ":")
-            expect(incoming[0].length, "to be greater than", 10)
+            expect(addressFn(incoming), "to be", true)
             done()
         })
     })
@@ -15,8 +15,7 @@ describe("proxylist", () => {
         tested.first().then((incoming) => {
             expect(incoming, "to be an array")
             expect(incoming.length, "to be greater than", 10)
-            expect(incoming[0], "to contain", ":")
-            expect(incoming[0].length, "to be greater than", 10)
+            expect(addressFn(incoming), "to be", true)
             done()
         })
     })
@@ -24,9 +23,20 @@ describe("proxylist", () => {
         tested.second().then((incoming) => {
             expect(incoming, "to be an array")
             expect(incoming.length, "to be greater than", 10)
-            expect(incoming[0], "to contain", ":")
-            expect(incoming[0].length, "to be greater than", 10)
+            expect(addressFn(incoming), "to be", true)
             done()
         })
     })
 })
+
+function addressFn(addressArr) {
+    
+    let flag = true
+    addressArr.map((address) => {
+        let state = R.match(/(?:[0-9\.\:])/g, address)
+        if (state.length !== address.length) {
+            flag = false
+        }
+    })
+    return flag
+}
